5b612ec0735cbfd52a9ce9048d23ce15db4df22d
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-08-24 08:46:40 -0300

    Adiconado navegaçoes dos botoes

---
+++ app/static/js/agenda.js
@@ -0,0 +1,43 @@
+const cabecalho = document.querySelector("#cabecalho")
+const menu = document.querySelector("#menu")
+const btn_home = document.querySelector("#btn_home")
+const btn_novo = document.querySelector("#btn_novo")
+const btn_pesquisar = document.querySelector("#btn_pesquisar")
+const btn_gestao = document.querySelector("#btn_gestao")
+const btn_sobre = document.querySelector("#btn_sobre")
+const principal = document.querySelector("#principal")
+const if_principal = document.querySelector('if_principal')
+
+btn_home.addEventListener('click',(evt)=>{
+    selecionarAba(evt.target,'static/html/home.html')
+})
+
+btn_novo.addEventListener('click',(evt)=>{
+    selecionarAba(evt.target,'static/html/novo.html')
+})
+
+btn_pesquisar.addEventListener('click',(evt)=>{
+    selecionarAba(evt.target,'static/html/pesquisar.html')
+
+})
+
+btn_gestao.addEventListener('click',(evt)=>{
+    selecionarAba(evt.target,'static/html/gestao.html')
+})
+
+btn_sobre.addEventListener('click',(evt)=>{
+    selecionarAba(evt.target,'static/html/sobre.html')
+})
+
+
+const selecionarAba=(el,url)=>{
+    const abas = [...document.querySelectorAll('.aba')]
+    abas.map(e=>{
+        e.classList.remove('abaSelecionada')
+    })
+    el.classList.add('abaSelecionada')
+    window.open(url,'if_principal')
+    if_principal.removeAttribute('src')
+    if_principal.setAttribute('src',url)
+
+}
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def Agenda(request):
+    return render(request,'agenda.html')
